4cdcbd24e663560a8d10e5d95f597b5f7b5397c4
Juankri <Juancri687@gmail.com>
2026-01-28 19:21:55 -0300

    Menu navegacion2.0

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,15 +1,23 @@
 const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.getElementById("navLinks");
+const links = navLinks.querySelectorAll("a");
 
 menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-    if(navLinks.classList.contains("active")){
+    const isActive = navLinks.classList.toggle("active");
+    document.body.classList.toggle("body-no-scroll", isActive);
+
+    if(isActive){
         menuBtn.innerHTML = "&#10005;"
         menuBtn.setAttribute("aria-expanded", "true")
-            } else {
-                menuBtn.innerHTML = "&#9776;";
-                menuBtn.setAttribute("aria-expanded", "false")
-            }
+    } else {
+        menuBtn.innerHTML = "&#9776;";
+        menuBtn.setAttribute("aria-expanded", "false")
+    }
+});
 
-            
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+        menuBtn.innerHTML = "&#9776;";
+        menuBtn.setAttribute("aria-expanded", "false")});
 });
